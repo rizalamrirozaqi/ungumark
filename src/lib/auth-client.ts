08a32989e0dfad8c0b3db4f3a01a58d703ea3aa6
rushiaremka <rushiaremka@gmail.com>
2026-05-27 17:28:43 +0700

    auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { env } from '$env/dynamic/public'; // 🔥 Pake ini biar kebaca di serve
 
 export const authClient = createAuthClient({
     // Sekarang dia bakal ngambil URL yang bener, baik saat dirakit Vercel maupun di browser
-    baseURL: env.PUBLIC_BETTER_AUTH_URL ?? 'http://localhost:5173',
+    // baseURL: env.PUBLIC_BETTER_AUTH_URL ?? 'http://localhost:5173',
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
