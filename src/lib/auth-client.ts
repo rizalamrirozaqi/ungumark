d6ed558dc9cf29f640db3d4c46af0ca8e3ff334c
rushiaremka <rushiaremka@gmail.com>
2026-05-27 20:14:07 +0700

    hapus dev di auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,8 @@
 import { createAuthClient } from 'better-auth/svelte';
 import { env } from '$env/dynamic/public'; // 🔥 Pake ini biar kebaca di server & client
 import { sentinelClient } from "@better-auth/infra/client";
-import { dev } from '$app/environment';
 
 export const authClient = createAuthClient({
-    baseURL: dev ? 'http://localhost:5173' : 'https://ungumark.my.id',
     plugins: [
         sentinelClient()
     ]
