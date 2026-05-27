659d61690806cc7efbfa1ad32b07436c35940b9f
rushiaremka <rushiaremka@gmail.com>
2026-05-27 19:39:02 +0700

    tambah plugin sentinelclient

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,11 @@
 import { createAuthClient } from 'better-auth/svelte';
 import { env } from '$env/dynamic/public'; // 🔥 Pake ini biar kebaca di server & client
+import { sentinelClient } from "@better-auth/infra/client";
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+    plugins: [
+        // ... other plugins
+        sentinelClient()
+    ]
+});
 export const { signIn, signUp, signOut, useSession } = authClient;
--- src/routes/api/auth/[...all]/+server.ts
+++ src/routes/api/auth/[...all]/+server.ts
@@ -3,3 +3,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = ({ request }) => auth.handler(request);
 export const POST: RequestHandler = ({ request }) => auth.handler(request);
+export const OPTIONS: RequestHandler = ({ request }) => auth.handler(request);
+export const PATCH: RequestHandler = ({ request }) => auth.handler(request);
+export const PUT: RequestHandler = ({ request }) => auth.handler(request);
+export const DELETE: RequestHandler = ({ request }) => auth.handler(request);
